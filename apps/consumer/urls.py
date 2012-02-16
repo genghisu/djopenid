2b14b2c521b157a18c745aaedae121be224e32ef
genghisu <genghisu@gmail.com>
2012-02-16 14:18:37 -0500

    updates

--- apps/consumer/urls.py
+++ apps/consumer/urls.py
@@ -5,5 +5,4 @@ urlpatterns = patterns(
     'consumer.views',
     (r'^$', 'startOpenID'),
     (r'^finish/$', 'finishOpenID'),
-    (r'^xrds/$', 'rpXRDS'),
 )
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
     '',
-    ('^$', 'core.views.index'),
     ('^consumer/', include('consumer.urls')),
-    ('^server/', include('server.urls')),
 )
