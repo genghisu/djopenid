ebad454544d0b2ef007b542a23d4f8d48739a6a7
genghisu <genghisu@gmail.com>
2012-02-22 20:41:55 -0500

    updates

--- apps/consumer/urls.py
+++ apps/consumer/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns(
     'consumer.views',
     (r'^$', 'startOpenID'),
     (r'^finish/$', 'finishOpenID'),
-    (r'^settings/$', 'settings'),
+    (r'^settings/$', 'serverSettings'),
 )
