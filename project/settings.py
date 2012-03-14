5cdfd43d495bc53046e3a2b92a52a442f2b2b07a
genghisu <genghisu@gmail.com>
2012-03-14 16:15:55 -0400

    updates

--- project/settings.py
+++ project/settings.py
@@ -69,7 +69,12 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'project.urls'
 
-TEMPLATE_CONTEXT_PROCESSORS = ()
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth', 
+#    'django.core.context_processors.static',,
+    'django.core.context_processors.debug', 
+    'django.core.context_processors.i18n', 
+)
 
 TEMPLATE_DIRS = (
     os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates')),
